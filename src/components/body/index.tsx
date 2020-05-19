d626553c14cf878391d49817b9e2f5cc77b51e33
dreplica <adejo.david@aiesec.net>
2020-05-19 15:40:57 +0100

    restructured body styleing, and added likns that ware shortened

--- src/components/body/index.tsx
+++ src/components/body/index.tsx
@@ -17,10 +17,9 @@ export default function Content() {
   return (
     <Container>
       <Links>
-         <ShortedLinks link="jambori.com" />
-         <ShortedLinks link="jambori.com" />
-         <ShortedLinks link="jambori.com" />
-         <ShortedLinks link="jambori.com" />
+        <ShortedLinks link="jambori.com" />
+        <ShortedLinks link="jambori.com" />
+        <ShortedLinks link="jambori.com" />
       </Links>
       <Article />
       <Cardbox>
