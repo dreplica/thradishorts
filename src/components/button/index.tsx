bc9e068c8e6f91a25abeed20fd0eafcc3f65333a
David <adejo.david@aiesec.net>
2020-05-23 00:30:06 +0100

    links come back on clicking shorten it

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,21 +1,29 @@
-import React from 'react';
-import {Text} from './style'
+import React from "react";
+import { Text } from "./style";
 
 interface Iprops {
-    padding?:{left:number,top?:number};
-    radius?:number;
-    text?: string;
-    width?: string;
-    height?:number;
+  padding?: { left: number; top?: number };
+  radius?: number;
+  text?: string;
+  width?: string;
+  height?: number;
+  onClick?: (arg: any) => void;
 }
-export default function Button(props:Iprops) {
-  return  <Text style={{
-        width:props.width,
-        height:props.height,
-        paddingLeft:props.padding?.left,
-        paddingRight:props.padding?.left,
-        paddingTop:props.padding?.top,
-        paddingBottom:props.padding?.top,
-        borderRadius:props.radius
-  }}>{props.text}</Text>
+export default function Button(props: Iprops) {
+  return (
+    <Text
+      style={{
+        width: props.width,
+        height: props.height,
+        paddingLeft: props.padding?.left,
+        paddingRight: props.padding?.left,
+        paddingTop: props.padding?.top,
+        paddingBottom: props.padding?.top,
+        borderRadius: props.radius,
+      }}
+      onClick={props.onClick}
+    >
+      {props.text}
+    </Text>
+  );
 }
--- src/components/intro/url/index.tsx
+++ src/components/intro/url/index.tsx
@@ -1,22 +1,41 @@
-import React from 'react';
+import React, { useState, ChangeEvent } from 'react';
+import Axios from 'axios'
 
 import Button from '../../button';
 import {Container} from './style'
 
 export default function Urlpath() {
+  const [state, setstate] = useState("")
+
+  const edit_state = (e:ChangeEvent<HTMLInputElement>)=>{
+   const val =  e.currentTarget.value
+   setstate(val)
+  }
+
+  const send_url = async (e:ChangeEvent<HTMLButtonElement>)=>{
+    e.preventDefault()
+    console.log(state)
+    const url = await Axios.post(`http://localhost:3000/shorten`,{url:state})
+    console.log(url.data)
+  }
+
   return (
     <Container>
         <form>
             <label> 
                 <input 
                 type='text' 
+                onChange={edit_state}
+                value={state}
                 placeholder='Shorten a link here...'
                 />
             </label>
         <Button 
             padding={{left:20,top:10}}
             radius={5}
+            width='80%'
             text='Shorten it!'
+            onClick={send_url}
             />
         </form>
     </Container>
