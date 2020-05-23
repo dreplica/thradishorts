5ad54ffd2f61ecb59d0cb0f7826023998479e8fd
David <adejo.david@aiesec.net>
2020-05-23 20:26:46 +0100

    links to page on heroku

--- src/components/body/result/index.tsx
+++ src/components/body/result/index.tsx
@@ -1,29 +1,33 @@
-import React, { useRef, useState, useEffect} from 'react';
+import React, { useRef, useState, useEffect } from "react";
 
-import { Container, Short,Copy } from './style';
+import { Container, Short, Copy } from "./style";
 
 interface Iprops {
-    link: string;
+  link: string;
 }
 
 export default function ShortedLinks(props: Iprops) {
-    const [state, setstate] = useState('copy')
-    useEffect(() => {
-        setstate("copy")
-    }, [props.link])
+  const [state, setstate] = useState("copy");
+  useEffect(() => {
+    setstate("copy");
+  }, [props.link]);
 
-    const ref = useRef<HTMLInputElement>(null)
-    const copied = () => {
-        ref.current?.select()
-        // ref.current?.setSelectionRange(0, 9999);
-        document.execCommand('copy') 
-        setstate('copied') 
-    }
+  const ref = useRef<HTMLInputElement>(null);
+  const copied = () => {
+    ref.current?.select();
+    // ref.current?.setSelectionRange(0, 9999);
+    document.execCommand("copy");
+    setstate("copied");
+  };
 
-    return (
-        <Container> 
-            <Short type='text' ref={ref} value={`http://localhost:3000/${props.link}`}/>
-            <Copy onClick={copied}>{state}</Copy>
-        </Container>
-    )
+  return (
+    <Container>
+      <Short
+        type="text"
+        ref={ref}
+        value={`https://thradishorts.herokuapp.com/${props.link}`}
+      />
+      <Copy onClick={copied}>{state}</Copy>
+    </Container>
+  );
 }
