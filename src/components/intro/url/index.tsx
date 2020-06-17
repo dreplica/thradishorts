ab73dcd53026adbe5b13c7f735e5f87546eafe83
David <adejo.david@aiesec.net>
2020-06-17 15:14:31 +0100

    fixed url button issue

--- src/components/intro/url/index.tsx
+++ src/components/intro/url/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, ChangeEvent } from "react";
+import React, { useState, useContext, ChangeEvent, useEffect } from "react";
 import Axios from "axios";
 
 import { ContextState } from "../../../App";
@@ -6,18 +6,30 @@ import Button from "../../button";
 import { Container } from "./style";
 
 export default function Urlpath() {
-  const [state, setstate] = useState("");
+  const [state, setstate] = useState({ val: "", width: "80%" });
   const { dispatch } = useContext(ContextState);
+
+  useEffect(() => {
+    window.addEventListener("resize", resizeLenght, false);
+    return () => {
+      window.removeEventListener("resize", resizeLenght, false);
+    };
+  }, [window.innerWidth]);
+
+  const resizeLenght = () =>
+    window.innerWidth >= 950
+      ? setstate({ ...state, width: "20%" })
+      : setstate({ ...state, width: "80%" });
 
   const edit_state = (e: ChangeEvent<HTMLInputElement>) => {
     const val = e.currentTarget.value;
-    setstate(val);
+    setstate({ ...state, val });
   };
 
   const send_url = async (e: ChangeEvent<HTMLButtonElement>) => {
     e.preventDefault();
     const url = await Axios.post(`https://thradishorts.herokuapp.com/shorten`, {
-      url: state,
+      url: state.val,
     });
     if (!url.data.error) {
       dispatch({ type: "New link", payload: url.data.url });
@@ -31,14 +43,14 @@ export default function Urlpath() {
           <input
             type="text"
             onChange={edit_state}
-            value={state}
+            value={state.val}
             placeholder="Shorten a link here..."
           />
         </label>
         <Button
           padding={{ left: 20, top: 10 }}
           radius={5}
-          width="80%"
+          width={state.width}
           text="Shorten it!"
           onClick={send_url}
         />
